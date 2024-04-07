a91cbe5c4aed0f0b895f94d166ebeb9b7767a57c
leodreizzen <leodreizzen@gmail.com>
2024-04-06 21:32:38 -0300

    Agregar postinstall para que instale dependencias de python y react, y agregar script para correr solo la api

--- pybuild.py
+++
@@ -1,5 +0,0 @@
-import os
-import subprocess
-
-python_folder = "./.venv/scripts" if os.name == "nt" else "./.venv/bin"
-subprocess.run([os.path.join(python_folder, "pyinstaller"), "api/api.py", "--onefile", "--distpath", os.path.join("build", "api")])
