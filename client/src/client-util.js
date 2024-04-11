a72b595e3fab8f44864ba05337305d2d6bc697d3
rocio <rociozentrigen@gmail.com>
2024-04-11 15:33:30 -0300

    Comienzo de grafico aceleracion y cambios en la GUI (+ archivos)

---
+++ client/src/client-util.js
@@ -0,0 +1,38 @@
+export function dateUTC_to_LocalTimezone(utcString:string) {
+    var utcDate = new Date(utcString);
+    var timezoneOffset = utcDate.getTimezoneOffset();
+    var localDate = new Date(utcDate.getTime() - (timezoneOffset * 60 * 1000));
+
+    return localDate.toLocaleString();
+}
+
+export function dateUTC_to_dateTime(utcString:string){
+    const utcDate = new Date(utcString);
+
+    const date = utcDate.toLocaleDateString();
+    const time = utcDate.toLocaleTimeString();
+    const timezoneOffset = utcDate.getTimezoneOffset();
+
+    return `${date} ${time} (UTC${timezoneOffset >= 0 ? '-' : '+'}${Math.abs(timezoneOffset / 60)})`;
+}
+
+export function dateUTC_to_timeUnit(utcString, timeUnitString){
+    var utcDate = new Date(utcString);
+    var timeInMilliseconds = utcDate.getTime();
+    var timeInUnit
+    switch (timeUnitString) {
+        case 'min':
+            timeInUnit = timeInMilliseconds / (1000 * 60);
+            break;
+        case 'h':
+            timeInUnit = timeInMilliseconds / (1000 * 60 * 60);
+            break;
+        case 's':
+            timeInUnit = timeInUnit = timeInMilliseconds / 1000;
+            break;
+        default:
+            timeInUnit = timeInMilliseconds * 1000000; //nanosegundos
+            break;
+    }
+    return timeInUnit;
+}
---
+++ client/src/plots/AccelerationPlot.js
@@ -0,0 +1,29 @@
+import Plot from 'react-plotly.js';
+import {OrbitProgress} from "react-loading-indicators";
+import {dateUTC_to_timeUnit} from "../client-util"
+
+export default function AccelerationPlot({className, isDataLoading, accelerationData, timeUnit}) {
+    console.log(isDataLoading? "Cargando" : accelerationData === null? "NO data" :
+        "Tiempo:" +  Number(dateUTC_to_timeUnit(accelerationData.map(it => it.time))) + "Aceleracion:" + accelerationData.map(it => it.acceleration.module));
+    return (
+        <div>
+            {isDataLoading ? <OrbitProgress/> :
+                accelerationData !== null ?
+                    <Plot
+                        data={[
+                            {
+                                x: Number(dateUTC_to_timeUnit(accelerationData.map(it => it.time), "min")),
+                                y: accelerationData.map(it => it.acceleration.module),
+                                type: 'scatter',
+                                mode: 'lines',
+                                marker: {color: 'red'},
+                            }
+                        ]}
+                        layout={{width: 600, height: 600, title:'Aceleración en función del tiempo'}}
+                    />
+                    : null}
+        </div>
+    )
+}
+
+/* .replace(".", ",") */
