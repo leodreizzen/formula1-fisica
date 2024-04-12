240e68b6e9d7fa88b0a2790e345de9bf019ffd8d
leodreizzen <leodreizzen@gmail.com>
2024-04-12 00:45:48 -0300

    Arreglar nombre de ejes de gráfico de aceleración

--- client/src/plots/SplitAccelerationPlot.js
+++ client/src/plots/SplitAccelerationPlot.js
@@ -41,19 +41,20 @@ function SplitAccelerationPlot({className, isDataLoading, accelerationData, time
                         layout={{
                             title:'Aceleraciones en función del tiempo',
                             xaxis: {title: 'Tiempo [' + timeUnit + "]"},
+                            xaxis2: {title: 'Tiempo [' + timeUnit + "]"},
+                            xaxis3: {title: 'Tiempo [' + timeUnit + "]"},
 
-                            yaxis1: {title: 'a [m/s²]'},
+                            yaxis1: {title: '|a| [m/s²]'},
                             yaxis2: {title: 'a tangencial [m/s²]'},
                             yaxis3: {title: 'a normal [m/s²]'},
 
-                            margins: {t: 0, b: 0, l: 0, r: 0},
-                            grid: { rows: 3, columns: 1 },
+
+                            grid: { rows: 3, columns: 1, pattern: 'independent'},
                             width: width,
                             height: height,
                             autosize:true,
-
                         }}
-                        config={{responsive: true, scrollZoom: true, autosizable: true}}
+                        config={{responsive: true, scrollZoom: true}}
                     />
                     :null}
         </div>
