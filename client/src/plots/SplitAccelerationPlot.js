f6b09911307e4c1a17f36739cad42dca350d8783
leodreizzen <leodreizzen@gmail.com>
2024-04-13 15:57:42 -0300

    Refactorizar y sincronizar ejes

--- client/src/plots/SplitAccelerationPlot.js
+++ client/src/plots/SplitAccelerationPlot.js
@@ -2,22 +2,28 @@ import Plot from 'react-plotly.js';
 import {useResizeDetector} from 'react-resize-detector';
 import {light} from "@mui/material/styles/createPalette";
 import {plotStyles} from "../styles";
-import {useMemo} from "react";
+import {useMemo, useState} from "react";
 
 function SplitAccelerationPlot({className, timeUnit, traces}) {
     const {moduleTrace, tangentialTrace, normalTrace} = traces;
     const {width, height, ref} = useResizeDetector();
 
-    console.log(moduleTrace)
 
-    // const xRange = useMemo(() => {
-    //     const xValues = moduleTrace.x;
-    //     return [Math.min(...xValues), Math.max(...xValues)];
-    // }, [moduleTrace]);
+    const xAxisFont = {
+        family: plotStyles.font.family,
+        size:18,
+        color: plotStyles.font.color
+    }
 
-    const plotLayout = useMemo(()=>{
+    const yAxisFont = {
+        family: plotStyles.font.family,
+        size:16,
+        color: plotStyles.font.color
+    }
+
+    const plotLayout = useMemo(() => {
         return {
-            plot_bgcolor:plotStyles.plot_bgcolor,
+            plot_bgcolor: plotStyles.plot_bgcolor,
             paper_bgcolor: plotStyles.paper_bgcolor,
             font: plotStyles.font,
 
@@ -26,18 +32,25 @@ function SplitAccelerationPlot({className, timeUnit, traces}) {
                 color: plotStyles.axisColor,
                 gridcolor: plotStyles.gridColor,
                 gridwidth: 1,
+                titlefont: xAxisFont
             },
             xaxis2: {
-                title: 'Tiempo [' + timeUnit + "]",
+                anchor: 'x',
+                matches: 'x',
                 color: plotStyles.axisColor,
                 gridcolor: plotStyles.gridColor,
                 gridwidth: 1,
+                title: 'Tiempo [' + timeUnit + "]",
+                titlefont: xAxisFont
             },
             xaxis3: {
-                title: 'Tiempo [' + timeUnit + "]",
+                anchor: 'x',
+                matches: 'x',
                 color: plotStyles.axisColor,
                 gridcolor: plotStyles.gridColor,
                 gridwidth: 1,
+                title: 'Tiempo [' + timeUnit + "]",
+                titlefont: xAxisFont
             },
 
             yaxis1: {
@@ -45,18 +58,24 @@ function SplitAccelerationPlot({className, timeUnit, traces}) {
                 color: plotStyles.axisColor,
                 gridcolor: plotStyles.gridColor,
                 gridwidth: 1,
+                titlefont: yAxisFont
+
             },
             yaxis2: {
                 title: 'a tangencial [m/s²]',
                 color: plotStyles.axisColor,
                 gridcolor: plotStyles.gridColor,
                 gridwidth: 1,
+                titlefont: yAxisFont
+
             },
             yaxis3: {
                 title: 'a normal [m/s²]',
                 color: plotStyles.axisColor,
                 gridcolor: plotStyles.gridColor,
                 gridwidth: 1,
+                titlefont: yAxisFont
+
             },
             margin: {t: 20},
 
@@ -72,13 +91,13 @@ function SplitAccelerationPlot({className, timeUnit, traces}) {
     return (
         <div ref={ref} className={className + " flex p-0"}>
             <Plot className="w-full h-full"
-                data={[
-                    {...moduleTrace, showlegend: false, xaxis: 'x1', yaxis: 'y1'},
-                    {...tangentialTrace, xaxis: 'x2', yaxis: 'y2', showlegend: false},
-                    {...normalTrace, xaxis: 'x3', yaxis: 'y3', showlegend: false},
-                ]}
-                layout={plotLayout}
-                config={{responsive: true, scrollZoom: true, displayModeBar: false}}
+                  data={[
+                      {...moduleTrace, showlegend: false, xaxis: 'x1', yaxis: 'y1'},
+                      {...tangentialTrace, xaxis: 'x2', yaxis: 'y2', showlegend: false},
+                      {...normalTrace, xaxis: 'x3', yaxis: 'y3', showlegend: false},
+                  ]}
+                  layout={plotLayout}
+                  config={{responsive: true, scrollZoom: true, displayModeBar: false}}
             />
         </div>
     )
