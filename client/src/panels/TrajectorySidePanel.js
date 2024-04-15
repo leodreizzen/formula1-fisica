33e4bc20950c9ba1e649c29ef357e01968b5fc4f
leodreizzen <leodreizzen@gmail.com>
2024-04-15 20:08:14 -0300

    Agregar datos a menú lateral

--- client/src/panels/TrajectorySidePanel.js
+++ client/src/panels/TrajectorySidePanel.js
@@ -8,9 +8,55 @@ export default function
             {hoveredPoint !== null ? (
                 <>
                     <p>Tiempo: {trajectoryData[hoveredPoint].time.match(/(\d{2}):(\d{2})\.(\d{2})/)[0]}</p>
-                    <p>x: {(trajectoryData[hoveredPoint].x / 10).toFixed(2)}m</p>
-                    <p>y: {(trajectoryData[hoveredPoint].y / 10).toFixed(2)}m</p>
-                    <p>z: {(trajectoryData[hoveredPoint].z / 10).toFixed(2)}m</p>
+                    <table className="table-auto">
+                        <thead>
+                        <tr>
+                            <th></th>
+                            <th>x</th>
+                            <th>y</th>
+                            <th>z</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th>r</th>
+                            <td>{(trajectoryData[hoveredPoint].x / 10).toFixed(2)}m</td>
+                            <td>{(trajectoryData[hoveredPoint].y / 10).toFixed(2)}m</td>
+                            <td>{(trajectoryData[hoveredPoint].z / 10).toFixed(2)}m</td>
+                        </tr>
+                        <tr>
+                            <th>v</th>
+                            {/*TODO VELOCIDAD*/}
+                            <td>{(trajectoryData[hoveredPoint].x / 10).toFixed(2)}m</td>
+                            {/*TODO  cambiar*/}
+                            <td>{(trajectoryData[hoveredPoint].y / 10).toFixed(2)}m</td>
+                            {/*TODO  cambiar*/}
+                        </tr>
+                        <tr>
+                            <th>a</th>
+                            {/*TODO ACELERACION}*/}
+                            <td>{(trajectoryData[hoveredPoint].x / 10).toFixed(2)}m</td>
+                            {/*TODO cambiar */}
+                            <td>{(trajectoryData[hoveredPoint].y / 10).toFixed(2)}m</td>
+                            {/*TODO cambiar */}
+                            <td>{(trajectoryData[hoveredPoint].z / 10).toFixed(2)}m</td>
+                            {/*TODO cambiar */}
+                        </tr>
+                        </tbody>
+                    </table>
+                    <div className="flex mt-2 items-center justify-end">
+                        <div className="w-1/2 flex flex-col text-left">
+                            <div className="grid grid-cols-2 ">
+                                <span>a tangencial:</span> <span>10</span>
+                                <span>a normal:</span> <span>10</span>
+                                <span>módulo a:</span> <span>10</span>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-2">
+                            <span>módulo v </span> <span>10</span>
+                            <span>velocímetro: </span> <span>10</span>
+                        </div>
+                    </div>
                     <MiniPlot className="w-full grow mt-4" trajectoryData={trajectoryData} hoveredPoint={hoveredPoint}/>
                 </>
             ) : null}
