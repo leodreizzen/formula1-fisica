f7923d0357463ea4521c42b9bd41ebf0c95c97d3
Juan Ignacio <juanirmariani@gmail.com>
2024-04-15 19:02:32 -0300

    Muestra de tiempo en panel trayectoria en minutos, segundos y milisegundos. Se minimizo la muestra de x,y z en el grafico lateral

--- client/src/panels/TrajectorySidePanel.js
+++ client/src/panels/TrajectorySidePanel.js
@@ -2,15 +2,16 @@ import {MiniPlot} from "./MiniPlot";
 
 export default function
     TrajectorySidePanel({className, trajectoryData, hoveredPoint}) {
+
     return <div className={className + " bg-[hsl(218,80,10)] rounded-[2.5rem] flex"}>
         <div className="m-auto">
             {hoveredPoint !== null ? (
                 <>
-                    <p>Tiempo: {trajectoryData[hoveredPoint].time}</p>
-                    <p>x: {trajectoryData[hoveredPoint].x/10}m</p>
-                    <p>y: {trajectoryData[hoveredPoint].y/10}m</p>
-                    <p>z: {trajectoryData[hoveredPoint].z/10}m</p>
-                    <MiniPlot trajectoryData={trajectoryData} hoveredPoint={hoveredPoint}/>
+                    <p>Tiempo: {trajectoryData[hoveredPoint].time.match(/(\d{2}):(\d{2})\.(\d{2})/)[0]}</p>
+                    <p>x: {(trajectoryData[hoveredPoint].x / 10).toFixed(2)}m</p>
+                    <p>y: {(trajectoryData[hoveredPoint].y / 10).toFixed(2)}m</p>
+                    <p>z: {(trajectoryData[hoveredPoint].z / 10).toFixed(2)}m</p>
+                    <MiniPlot className trajectoryData={trajectoryData} hoveredPoint={hoveredPoint}/>
                 </>
             ) : null}
         < /div>
