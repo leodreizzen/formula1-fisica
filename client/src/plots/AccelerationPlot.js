15bd036a9b1d5e8ea0c9b24310ea04f31a43c114
leodreizzen <leodreizzen@gmail.com>
2024-04-14 23:22:48 -0300

    Optimizar

--- client/src/plots/AccelerationPlot.js
+++ client/src/plots/AccelerationPlot.js
@@ -2,29 +2,38 @@ import OverlappingAccelerationPlot from "./OverlappingAccelerationPlot";
 import SplitAccelerationPlot from "./SplitAccelerationPlot";
 import {timeDeltaToTimeUnit} from "../client-util";
 import {OrbitProgress} from "react-loading-indicators";
+import {useMemo} from "react";
 
 export default function AccelerationPlot({className, isDataLoading, accelerationData, timeUnit}) {
-    const moduleTrace = {
-        x: accelerationData?.map(it => timeDeltaToTimeUnit(it.time, timeUnit)),
-        y: accelerationData?.map(it => it.acceleration.module / 10),
-        type: 'scatter',
-        mode: 'lines',
-        marker: {color: 'red'},
-    };
-    const tangentialTrace = {
-        x: accelerationData?.map(it => timeDeltaToTimeUnit(it.time, timeUnit)),
-        y: accelerationData?.map(it => it.acceleration.aTangential / 10),
-        type: 'scatter',
-        mode: 'lines',
-        marker: {color: 'orange'},
-    };
-    const normalTrace = {
-        x: accelerationData?.map(it => timeDeltaToTimeUnit(it.time, timeUnit)),
-        y: accelerationData?.map(it => it.acceleration.aNormal / 10),
-        type: 'scatter',
-        mode: 'lines',
-        marker: {color: 'blue'},
-    };
+    const moduleTrace = useMemo(() => {
+            return {
+                x: accelerationData?.map(it => timeDeltaToTimeUnit(it.time, timeUnit)),
+                y: accelerationData?.map(it => it.acceleration.module / 10),
+                type: 'scatter',
+                mode: 'lines',
+                marker: {color: 'red'},
+            }
+        }, [accelerationData, timeUnit]);
+
+    const tangentialTrace = useMemo(()=>{
+        return {
+            x: accelerationData?.map(it => timeDeltaToTimeUnit(it.time, timeUnit)),
+            y: accelerationData?.map(it => it.acceleration.aTangential / 10),
+            type: 'scatter',
+            mode: 'lines',
+            marker: {color: 'orange'},
+        }
+    }, [accelerationData, timeUnit]);
+    const normalTrace = useMemo(()=>{
+        return {
+
+            x: accelerationData?.map(it => timeDeltaToTimeUnit(it.time, timeUnit)),
+            y: accelerationData?.map(it => it.acceleration.aNormal / 10),
+            type: 'scatter',
+            mode: 'lines',
+            marker: {color: 'blue'},
+        }
+    }, [accelerationData, timeUnit]);
 
     const traces = {moduleTrace, tangentialTrace, normalTrace};
 
