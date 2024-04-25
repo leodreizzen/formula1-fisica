e10be9c236ca0321150d1b8ae1b9161500238cbd
Franco Feuilles <francofeuilles@gmail.com>
2024-04-25 11:01:27 -0300

    Obtengo datos de ambos conductores y los muestro

--- client/src/components/panels/main/comparative/ComparativePanel.js
+++ client/src/components/panels/main/comparative/ComparativePanel.js
@@ -3,9 +3,23 @@ import DriverSelector from "../../../inputs/DriverSelector";
 import DriverSelectorNoContext from "../../../inputs/DriverSelectorNoContext";
 import LapSelector from "../../../inputs/LapSelector";
 import ComparativeSpeedPlot from "../../../plots/ComparativeSpeedPlot";
+import {useSessionDataContext} from "../../../../context/SessionDataContext";
+import {useDriverContext} from "../../../../context/DriverContext";
+import {useLapContext} from "../../../../context/LapContext";
+import {useGetTrajectory} from "../../../../api/hooks";
 
 export default function ComparativePanel({ className }) {
     const [currentSecondaryDriver, setCurrentSecondaryDriver] = useState(null);
+    const sessionData = useSessionDataContext();
+    const {currentDriver} = useDriverContext();
+    const {currentLap} = useLapContext();
+
+    const year = sessionData.year;
+    const round = sessionData.round;
+    const session = sessionData.session;
+
+    const [trajectoryData,] = useGetTrajectory(year, round, session, currentDriver?.driverNumber, currentLap);
+    const [trajectorySecondaryData,] = useGetTrajectory(year, round, session, currentSecondaryDriver?.driverNumber, currentLap);
 
     const handleSecondaryDriverChange = (driver) => {
         setCurrentSecondaryDriver(driver);
@@ -17,7 +31,7 @@ export default function ComparativePanel({ className }) {
                 <DriverSelector className="w-1/2" />
                 <DriverSelectorNoContext className="w-1/2" currentSecondaryDriver={currentSecondaryDriver} onDriverChange={handleSecondaryDriverChange} />
             </div>
-            <ComparativeSpeedPlot className="grow pt-2" timeUnit={"s"} currentSecondaryDriver={currentSecondaryDriver} />
+            <ComparativeSpeedPlot className="grow pt-2" timeUnit={"s"} trajectoryData={trajectoryData} trajectorySecondaryData={trajectorySecondaryData} currentDriver={currentDriver} currentSecondaryDriver={currentSecondaryDriver} currentLap={currentLap} />
             <LapSelector className="mb-3 p-1 pl-6 pr-6" />
         </div>
     );
--- client/src/components/plots/ComparativeSpeedPlot.js
+++ client/src/components/plots/ComparativeSpeedPlot.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useVectorsContext } from "../../context/VectorsContext";
 
-export default function ComparativeSpeedPlot({ className, timeUnit, currentSecondaryDriver }) {
-    const { vectors } = useVectorsContext();
+
+export default function ComparativeSpeedPlot({ className, timeUnit, trajectoryData, trajectorySecondaryData, currentDriver, currentSecondaryDriver, currentLap }) {
+    const { vectors } = useVectorsContext(); 
     const [secondaryDriverName, setSecondaryDriverName] = useState(null);
 
     useEffect(() => {
-        if (currentSecondaryDriver != null && currentSecondaryDriver.fullName) {
-            setSecondaryDriverName(`Conductor secundario: ${currentSecondaryDriver.fullName}`);
+        if (currentSecondaryDriver != null && currentDriver != null && currentSecondaryDriver.fullName && currentDriver.fullName) {
+            setSecondaryDriverName(`${currentDriver.fullName} vs. ${currentSecondaryDriver.fullName} en la vuelta: ${currentLap}`);
         } else {
             setSecondaryDriverName("Ningún conductor secundario seleccionado");
         }
-    }, [currentSecondaryDriver]);
+    }, [currentSecondaryDriver, currentDriver, currentLap]);
 
     return (
         <div className={className}>
