83fd47c269f67a3956d98669ffdbf47bdc206f4f
Franco Feuilles <francofeuilles@gmail.com>
2024-05-02 09:56:03 -0300

    Solución al caso donde ambos conductores son iguales

--- client/src/components/plots/ComparativePlots.js
+++ client/src/components/plots/ComparativePlots.js
@@ -15,8 +15,7 @@ export default function ComparativePlots({ className, trajectoryData, trajectory
 
     const plotData = useMemo(() => {
         if (trajectoryData && trajectorySecondaryData && vectors) {
-            return [
-            {
+            const primaryDriverData = {
                 x: trajectoryData.map(it => it.intrinsic.s / 10),
                 y: vectors.map(it => it.velocity.module / 10),
                 type: 'scatter',
@@ -28,48 +27,30 @@ export default function ComparativePlots({ className, trajectoryData, trajectory
                 visible: visible[0],
                 legendgroup: currentDriver.fullName,
                 showlegend: true
-            },
-            {
+            };
+    
+            const secondaryDriverData = {
                 x: trajectorySecondaryData.map(it => it.intrinsic.s / 10),
                 y: vectors.map(it => it.velocity.module / 10),
-                type: 'scatter',
-                mode: 'lines',
-                marker: { color: secondaryDriverColor},
-                name: currentSecondaryDriver.fullName,
-                xaxis: 'x1', 
-                yaxis: 'y1',
-                visible: visible[1],
-                legendgroup: currentSecondaryDriver.fullName,
-                showlegend: true
-            },
-            {
-                x: trajectoryData.map(it => it.intrinsic.s / 10),
-                y: vectors.map(it => it.acceleration.module / 10),
-                type: 'scatter',
-                mode: 'lines',
-                marker: { color: primaryDriverColor },
-                name: currentDriver.fullName,
-                xaxis: 'x2', 
-                yaxis: 'y2',
-                visible: visible[0],
-                legendgroup: currentDriver.fullName,
-                showlegend: false
-            },
-            {
-                x: trajectorySecondaryData.map(it => it.intrinsic.s / 10),
-                y: vectors.map(it => it.acceleration.module / 10),
                 type: 'scatter',
                 mode: 'lines',
                 marker: { color: secondaryDriverColor },
                 name: currentSecondaryDriver.fullName,
-                xaxis: 'x2', 
-                yaxis: 'y2',
+                xaxis: 'x1', 
+                yaxis: 'y1',
                 visible: visible[1],
                 legendgroup: currentSecondaryDriver.fullName,
-                showlegend: false
-            }];
+                showlegend: true
+            };
+    
+            return currentDriver === currentSecondaryDriver
+                ? [primaryDriverData, { ...primaryDriverData, y: vectors.map(it => it.acceleration.module / 10), xaxis: 'x2', yaxis: 'y2', showlegend: false }]
+                : [primaryDriverData, secondaryDriverData, 
+                    { ...primaryDriverData, y: vectors.map(it => it.acceleration.module / 10), xaxis: 'x2', yaxis: 'y2', showlegend: false },
+                    { ...secondaryDriverData, y: vectors.map(it => it.acceleration.module / 10), xaxis: 'x2', yaxis: 'y2', showlegend: false }];
         }
     }, [trajectoryData, trajectorySecondaryData, vectors, currentDriver, currentSecondaryDriver, visible]);
+    
 
     const plotLayout = useMemo(() => {
         const xAxisFont = {
