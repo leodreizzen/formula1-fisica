9de12a5b2b3c2df5b85acf9c3666fa2ae05478b0
Franco Feuilles <francofeuilles@gmail.com>
2024-05-02 16:11:27 -0300

    Ahora el también se puede hacer click en el label del radio button

--- client/src/components/inputs/AccelerationTypeSelector.js
+++ client/src/components/inputs/AccelerationTypeSelector.js
@@ -5,33 +5,36 @@ export default function AccelerationTypeSelector({ onChange, value }) {
         <div className="flex flex-row w-full">
             <div>
                 <input
+                    id="module"
                     type="radio"
                     value="module"
                     checked={value === "module"}
                     onChange={onChange}
                     className="mr-2 ml-4 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
-                <label className="radio-label text-lg text-gray-900 dark:text-gray-300">Modulo</label>
+                <label htmlFor="module" className="radio-label text-lg text-gray-900 dark:text-gray-300">Modulo</label>
             </div>
             <div>
                 <input
+                    id="tangential"
                     type="radio"
                     value="tangential"
                     checked={value === "tangential"}
                     onChange={onChange}
                     className="mr-2 ml-3 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
-                <label className="radio-label text-lg text-gray-900 dark:text-gray-300">Tangencial</label>
+                <label htmlFor="tangential" className="radio-label text-lg text-gray-900 dark:text-gray-300">Tangencial</label>
             </div>
             <div>
                 <input
+                    id="normal"
                     type="radio"
                     value="normal"
                     checked={value === "normal"}
                     onChange={onChange}
                     className="mr-2 ml-3 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
-                <label className="radio-label text-lg text-gray-900 dark:text-gray-300">Normal</label>
+                <label htmlFor="normal" className="radio-label text-lg text-gray-900 dark:text-gray-300">Normal</label>
             </div>
             {/* Estilos agregados a los labels */}
             <style>{`
