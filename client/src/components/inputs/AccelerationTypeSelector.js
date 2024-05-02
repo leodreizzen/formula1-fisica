f564e4a773224d7e3b2c3d8af61b96c917c4bacd
Franco Feuilles <francofeuilles@gmail.com>
2024-05-02 11:36:03 -0300

    Agrega estilos al AccelerationTypeSelector

--- client/src/components/inputs/AccelerationTypeSelector.js
+++ client/src/components/inputs/AccelerationTypeSelector.js
@@ -1,38 +1,46 @@
-
+import { plotStyles } from "../../styles";
 
 export default function AccelerationTypeSelector({ onChange, value }) {
     return (
         <div className="flex flex-row w-full">
-            <label>
+            <div>
                 <input
                     type="radio"
                     value="module"
                     checked={value === "module"}
                     onChange={onChange}
-                    className="mr-2 ml-4"
+                    className="mr-2 ml-4 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
-                Modulo
-            </label>
-            <label>
+                <label className="radio-label text-lg text-gray-900 dark:text-gray-300">Modulo</label>
+            </div>
+            <div>
                 <input
                     type="radio"
                     value="tangential"
                     checked={value === "tangential"}
                     onChange={onChange}
-                    className="mr-2 ml-3"
+                    className="mr-2 ml-3 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
-                Tangencial
-            </label>
-            <label>
+                <label className="radio-label text-lg text-gray-900 dark:text-gray-300">Tangencial</label>
+            </div>
+            <div>
                 <input
                     type="radio"
                     value="normal"
                     checked={value === "normal"}
                     onChange={onChange}
-                    className="mr-2 ml-3"
+                    className="mr-2 ml-3 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
-                Normal
-            </label>
+                <label className="radio-label text-lg text-gray-900 dark:text-gray-300">Normal</label>
+            </div>
+            {/* Estilos agregados a los labels */}
+            <style>{`
+                .radio-label {
+                    font-family: ${plotStyles.font.family};
+                    font-size: ${plotStyles.font.size}px;
+                    color: ${plotStyles.font.color};
+                }
+            `}</style>
         </div>
     );
 }
