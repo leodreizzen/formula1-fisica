95019660dbc3e6625007d282478afbf39f156c4b
leodreizzen <leodreizzen@gmail.com>
2024-05-04 22:13:03 -0300

    Marcar opción seleccionada

--- client/src/components/plots/PositionsPlot.js
+++ client/src/components/plots/PositionsPlot.js
@@ -46,19 +46,19 @@ export function PositionsPlot({className, timeUnit}) {
                     <input type="radio" id="cartesian" name="coordinateSystem" value="cartesian" className="hidden"
                            checked={coordinateSystem === "cartesian"} onChange={handleCoordinateSystemChange}/>
                     <label htmlFor="cartesian"
-                           className="p-2 border border-white rounded-full cursor-pointer bg-white text-black">Cartesianas</label>
+                           className={"no-select p-2 border border-white rounded-full cursor-pointer bg-white text-black " + (coordinateSystem === "cartesian" ? "bg-blue-600 text-white " : "")}>Cartesianas</label>
                 </div>
                 <div className="mt-6">
                     <input type="radio" id="polar" name="coordinateSystem" value="polar" className="hidden"
                            checked={coordinateSystem === "polar"} onChange={handleCoordinateSystemChange}/>
                     <label htmlFor="polar"
-                           className="p-2 border border-white rounded-full cursor-pointer bg-white text-black">Polares</label>
+                           className={"no-select p-2 border border-white rounded-full cursor-pointer bg-white text-black " + (coordinateSystem === "polar" ? "bg-blue-600 text-white " : "")}>Polares</label>
                 </div>
                 <div className="mt-6">
                     <input type="radio" id="intrinsic" name="coordinateSystem" value="intrinsic" className="hidden"
                            checked={coordinateSystem === "intrinsic"} onChange={handleCoordinateSystemChange}/>
                     <label htmlFor="intrinsic"
-                           className="p-2 border border-white rounded-full cursor-pointer bg-white text-black">Intrínsecas</label>
+                           className={"no-select p-2 border border-white rounded-full cursor-pointer bg-white text-black " + (coordinateSystem === "intrinsic" ? "bg-blue-600 text-white " : "")}>Intrínsecas</label>
                 </div>
             </div>
             {trajectoryData !== null ?
