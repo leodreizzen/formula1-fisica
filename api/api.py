717d7f0027de9650becd033bab8f381c5ea0d8cd
Julian Gallardo <juliangallardo26603@gmail.com>
2024-05-05 13:31:00 -0300

    Refactor de accelerations, agregado metodo accelerations_calcs para usar en derrapes y agregada lru_Cache

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 import math
+from functools import lru_cache
 
 import fastapi
 import json
@@ -12,12 +13,12 @@ from scipy.signal import savgol_filter
 import pandas as pd
 import numpy as np
 
-
-
 app = fastapi.FastAPI()
 facade = FastF1Facade()
 
 origins = ["*"]
+
+tamano_cache = 1
 
 # radio de giro para umbral = 7.35metros
 wheelbase = 3.6
@@ -94,66 +95,7 @@ def trajectory(year: int, roundNumber: int, sessionNumber: int, driverNumber: in
 
 @app.get("/vectors")
 def accelerations(year: int, roundNumber: int, sessionNumber: int, driverNumber: int, lapNumber: int):
-    # Parámetros del filtro de Savitzky-Golay
-    window_length_speed = 15
-    polyorder_speed= 2
-    window_length_acceleration = 100
-    polyorder_acceleration = 3
-
-    lap_telemetry = facade.telemetry(year, roundNumber, sessionNumber, driverNumber, lapNumber)
-    lap_telemetry['diferencia_tiempo'] = (lap_telemetry['Time'].diff().apply(lambda x: x.total_seconds())).fillna(0)
-    lap_telemetry['velocidad_x'] = (lap_telemetry['X'].diff() / lap_telemetry['diferencia_tiempo']).fillna(0)
-    lap_telemetry['velocidad_y'] = (lap_telemetry['Y'].diff() / lap_telemetry['diferencia_tiempo']).fillna(0)
-    lap_telemetry['velocidad_z'] = (lap_telemetry['Z'].diff() / lap_telemetry['diferencia_tiempo']).fillna(0)
-    # Filtrar velocidad con un filtro de Savitzky-Golay
-    lap_telemetry['velocidad_x'] = savgol_filter(lap_telemetry['velocidad_x'], window_length_speed, polyorder_speed)
-    lap_telemetry['velocidad_y'] = savgol_filter(lap_telemetry['velocidad_y'], window_length_speed, polyorder_speed)
-    lap_telemetry['velocidad_z'] = savgol_filter(lap_telemetry['velocidad_z'], window_length_speed, polyorder_speed)
-    lap_telemetry['aceleracion_x'] = ((lap_telemetry['velocidad_x'].shift(-1) - lap_telemetry['velocidad_x']) / lap_telemetry['diferencia_tiempo']).fillna(0).replace([np.inf, -np.inf], 0)
-    lap_telemetry['aceleracion_y'] = ((lap_telemetry['velocidad_y'].shift(-1) - lap_telemetry['velocidad_y']) / lap_telemetry['diferencia_tiempo']).fillna(0).replace([np.inf, -np.inf], 0)
-    lap_telemetry['aceleracion_z'] = ((lap_telemetry['velocidad_z'].shift(-1) - lap_telemetry['velocidad_z']) / lap_telemetry['diferencia_tiempo']).fillna(0).replace([np.inf, -np.inf], 0)
-    # Filtrar aceleración con un filtro de Savitzky-Golay
-    lap_telemetry['aceleracion_x'] = savgol_filter(lap_telemetry['aceleracion_x'], window_length_acceleration, polyorder_acceleration)
-    lap_telemetry['aceleracion_y'] = savgol_filter(lap_telemetry['aceleracion_y'], window_length_acceleration, polyorder_acceleration)
-    lap_telemetry['aceleracion_z'] = savgol_filter(lap_telemetry['aceleracion_z'], window_length_acceleration, polyorder_acceleration)
-    lap_telemetry['modulo_velocidad_xy'] = np.linalg.norm(lap_telemetry[['velocidad_x', 'velocidad_y']], axis=1)
-    lap_telemetry['modulo_velocidad'] = np.linalg.norm(lap_telemetry[['velocidad_x', 'velocidad_y', "velocidad_z"]], axis=1)
-    lap_telemetry['modulo_aceleracion'] = np.linalg.norm(lap_telemetry[['aceleracion_x', 'aceleracion_y', "aceleracion_z"]], axis=1)
-    lap_telemetry['modulo_aceleracion_xy'] = np.linalg.norm(lap_telemetry[['aceleracion_x', 'aceleracion_y']], axis=1)
-
-    # Calculamos el versor tangente para cada fila
-    versor_x_tangente = (lap_telemetry['velocidad_x'] / lap_telemetry['modulo_velocidad_xy']).fillna(0)
-    versor_y_tangente = (lap_telemetry['velocidad_y'] / lap_telemetry['modulo_velocidad_xy']).fillna(0)
-
-    # Creamos una nueva columna para el versor tangente
-    lap_telemetry['versor_tangente'] = list(zip(versor_x_tangente, versor_y_tangente))
-
-    lap_telemetry['aTangential'] = (
-            lap_telemetry['aceleracion_x'] * lap_telemetry['versor_tangente'].apply(lambda x: x[0]) +
-            lap_telemetry['aceleracion_y'] * lap_telemetry['versor_tangente'].apply(lambda x: x[1]))
-
-    lap_telemetry['versor_normal_x'] = -lap_telemetry['versor_tangente'].apply(lambda x: x[1])
-    lap_telemetry['versor_normal_y'] = lap_telemetry['versor_tangente'].apply(lambda x: x[0])
-
-    # Calculamos la aceleración normal para cada fila
-    lap_telemetry['a_normal'] = ((lap_telemetry['aceleracion_x'] * lap_telemetry['versor_normal_x']) +
-                (lap_telemetry['aceleracion_y'] * lap_telemetry['versor_normal_y']))
-
-    # Si la aceleración normal es negativa, invertimos el versor normal y la aceleración normal
-    a_negativa = lap_telemetry['a_normal'] < 0
-    lap_telemetry.loc[a_negativa, 'a_normal'] *= -1
-    lap_telemetry.loc[a_negativa, 'versor_normal_x'] *= -1
-    lap_telemetry.loc[a_negativa, 'versor_normal_y'] *= -1
-
-    # Eliminar la primera fila
-    lap_telemetry = lap_telemetry.iloc[1:]
-
-    # Eliminar la última fila
-    lap_telemetry = lap_telemetry.iloc[:-1]
-
-    # Le sacamos la primera fila(primer punto de la vuelta) y la ultima fila(ultimo punto) para que no haya NaN ni valores en Infinito
-
-    # Filtrar los datos con un filtro de Savitzky-Golay
+    lap_telemetry = accelerations_calcs(year, roundNumber, sessionNumber, driverNumber, lapNumber)
 
     aceleraciones = []
 
@@ -191,16 +133,17 @@ def accelerations(year: int, roundNumber: int, sessionNumber: int, driverNumber:
 
     return aceleraciones
 
+
 @app.get("/derrapes")
 def drifts(year: int, round_number: int, session_number: int, driver_number: int, lap_number: int):
-    datos_aceleraciones = pd.DataFrame(accelerations(year, round_number, session_number, driver_number, lap_number))
+    datos_aceleraciones = accelerations_calcs(year, round_number, session_number, driver_number, lap_number)
     seleccion = []
 
     for index, row in datos_aceleraciones.iterrows():
         derrape = {}
         #pasaje de dm a m
-        velocidad = row["speed"]["module"]/10
-        aceleracion_normal = row["acceleration"]["aNormal"]/10
+        velocidad = row["speed"]["module"] / 10
+        aceleracion_normal = row["acceleration"]["aNormal"] / 10
         if aceleracion_normal != 0:
             radio = (velocidad ** 2) / aceleracion_normal
             if radio < radio_giro_minimo:
@@ -211,6 +154,83 @@ def drifts(year: int, round_number: int, session_number: int, driver_number: int
                 seleccion.append(row)
 
     return seleccion
+
+
+@lru_cache(maxsize=tamano_cache)
+def accelerations_calcs(year: int, roundNumber: int, sessionNumber: int, driverNumber: int, lapNumber: int):
+    # Parámetros del filtro de Savitzky-Golay
+    window_length_speed = 15
+    polyorder_speed = 2
+    window_length_acceleration = 100
+    polyorder_acceleration = 3
+
+    lap_telemetry = facade.telemetry(year, roundNumber, sessionNumber, driverNumber, lapNumber)
+    lap_telemetry['diferencia_tiempo'] = (lap_telemetry['Time'].diff().apply(lambda x: x.total_seconds())).fillna(0)
+    lap_telemetry['velocidad_x'] = (lap_telemetry['X'].diff() / lap_telemetry['diferencia_tiempo']).fillna(0)
+    lap_telemetry['velocidad_y'] = (lap_telemetry['Y'].diff() / lap_telemetry['diferencia_tiempo']).fillna(0)
+    lap_telemetry['velocidad_z'] = (lap_telemetry['Z'].diff() / lap_telemetry['diferencia_tiempo']).fillna(0)
+    # Filtrar velocidad con un filtro de Savitzky-Golay
+    lap_telemetry['velocidad_x'] = savgol_filter(lap_telemetry['velocidad_x'], window_length_speed, polyorder_speed)
+    lap_telemetry['velocidad_y'] = savgol_filter(lap_telemetry['velocidad_y'], window_length_speed, polyorder_speed)
+    lap_telemetry['velocidad_z'] = savgol_filter(lap_telemetry['velocidad_z'], window_length_speed, polyorder_speed)
+    lap_telemetry['aceleracion_x'] = (
+            (lap_telemetry['velocidad_x'].shift(-1) - lap_telemetry['velocidad_x']) / lap_telemetry[
+        'diferencia_tiempo']).fillna(0).replace([np.inf, -np.inf], 0)
+    lap_telemetry['aceleracion_y'] = (
+            (lap_telemetry['velocidad_y'].shift(-1) - lap_telemetry['velocidad_y']) / lap_telemetry[
+        'diferencia_tiempo']).fillna(0).replace([np.inf, -np.inf], 0)
+    lap_telemetry['aceleracion_z'] = (
+            (lap_telemetry['velocidad_z'].shift(-1) - lap_telemetry['velocidad_z']) / lap_telemetry[
+        'diferencia_tiempo']).fillna(0).replace([np.inf, -np.inf], 0)
+    # Filtrar aceleración con un filtro de Savitzky-Golay
+    lap_telemetry['aceleracion_x'] = savgol_filter(lap_telemetry['aceleracion_x'], window_length_acceleration,
+                                                   polyorder_acceleration)
+    lap_telemetry['aceleracion_y'] = savgol_filter(lap_telemetry['aceleracion_y'], window_length_acceleration,
+                                                   polyorder_acceleration)
+    lap_telemetry['aceleracion_z'] = savgol_filter(lap_telemetry['aceleracion_z'], window_length_acceleration,
+                                                   polyorder_acceleration)
+    lap_telemetry['modulo_velocidad_xy'] = np.linalg.norm(lap_telemetry[['velocidad_x', 'velocidad_y']], axis=1)
+    lap_telemetry['modulo_velocidad'] = np.linalg.norm(lap_telemetry[['velocidad_x', 'velocidad_y', "velocidad_z"]],
+                                                       axis=1)
+    lap_telemetry['modulo_aceleracion'] = np.linalg.norm(
+        lap_telemetry[['aceleracion_x', 'aceleracion_y', "aceleracion_z"]], axis=1)
+    lap_telemetry['modulo_aceleracion_xy'] = np.linalg.norm(lap_telemetry[['aceleracion_x', 'aceleracion_y']], axis=1)
+
+    # Calculamos el versor tangente para cada fila
+    versor_x_tangente = (lap_telemetry['velocidad_x'] / lap_telemetry['modulo_velocidad_xy']).fillna(0)
+    versor_y_tangente = (lap_telemetry['velocidad_y'] / lap_telemetry['modulo_velocidad_xy']).fillna(0)
+
+    # Creamos una nueva columna para el versor tangente
+    lap_telemetry['versor_tangente'] = list(zip(versor_x_tangente, versor_y_tangente))
+
+    lap_telemetry['aTangential'] = (
+            lap_telemetry['aceleracion_x'] * lap_telemetry['versor_tangente'].apply(lambda x: x[0]) +
+            lap_telemetry['aceleracion_y'] * lap_telemetry['versor_tangente'].apply(lambda x: x[1]))
+
+    lap_telemetry['versor_normal_x'] = -lap_telemetry['versor_tangente'].apply(lambda x: x[1])
+    lap_telemetry['versor_normal_y'] = lap_telemetry['versor_tangente'].apply(lambda x: x[0])
+
+    # Calculamos la aceleración normal para cada fila
+    lap_telemetry['a_normal'] = ((lap_telemetry['aceleracion_x'] * lap_telemetry['versor_normal_x']) +
+                                 (lap_telemetry['aceleracion_y'] * lap_telemetry['versor_normal_y']))
+
+    # Si la aceleración normal es negativa, invertimos el versor normal y la aceleración normal
+    a_negativa = lap_telemetry['a_normal'] < 0
+    lap_telemetry.loc[a_negativa, 'a_normal'] *= -1
+    lap_telemetry.loc[a_negativa, 'versor_normal_x'] *= -1
+    lap_telemetry.loc[a_negativa, 'versor_normal_y'] *= -1
+
+    # Eliminar la primera fila
+    lap_telemetry = lap_telemetry.iloc[1:]
+
+    # Eliminar la última fila
+    lap_telemetry = lap_telemetry.iloc[:-1]
+
+    # Le sacamos la primera fila(primer punto de la vuelta) y la ultima fila(ultimo punto) para que no haya NaN ni valores en Infinito
+
+    # Filtrar los datos con un filtro de Savitzky-Golay
+
+    return lap_telemetry
 
 
 if __name__ == "__main__":
