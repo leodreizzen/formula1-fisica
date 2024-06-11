13257be5dd055edbe2aeb9bde5a10dcf0a97366b
Franco Feuilles <francofeuilles@gmail.com>
2024-06-11 10:18:08 -0300

    Agrega el componente "CoordinateSystemSelector"

---
+++ client/src/components/inputs/CoordinateSystemSelector.js
@@ -0,0 +1,63 @@
+export function CoordinateSystemSelector({ className, coordinateSystem, onCoordinateSystemChange }) {
+    return (
+        <div className={className + " flex flex-col items-start"}>
+            <div className="mt-6 w-full flex justify-center">
+                <input
+                    type="radio"
+                    id="cartesian"
+                    name="coordinateSystem"
+                    value="cartesian"
+                    className="hidden"
+                    checked={coordinateSystem === "cartesian"}
+                    onChange={onCoordinateSystemChange}
+                />
+                <label
+                    htmlFor="cartesian"
+                    className={`no-select flex items-center justify-center p-2 border border-white rounded-full cursor-pointer w-28 ${
+                        coordinateSystem === "cartesian" ? "bg-blue-600 text-white" : "bg-white text-black"
+                    }`}
+                >
+                    Cartesianas
+                </label>
+            </div>
+            <div className="mt-6 w-full flex justify-center">
+                <input
+                    type="radio"
+                    id="polar"
+                    name="coordinateSystem"
+                    value="polar"
+                    className="hidden"
+                    checked={coordinateSystem === "polar"}
+                    onChange={onCoordinateSystemChange}
+                />
+                <label
+                    htmlFor="polar"
+                    className={`no-select flex items-center justify-center p-2 border border-white rounded-full cursor-pointer w-28 ${
+                        coordinateSystem === "polar" ? "bg-blue-600 text-white" : "bg-white text-black"
+                    }`}
+                >
+                    Polares
+                </label>
+            </div>
+            <div className="mt-6 w-full flex justify-center">
+                <input
+                    type="radio"
+                    id="intrinsic"
+                    name="coordinateSystem"
+                    value="intrinsic"
+                    className="hidden"
+                    checked={coordinateSystem === "intrinsic"}
+                    onChange={onCoordinateSystemChange}
+                />
+                <label
+                    htmlFor="intrinsic"
+                    className={`no-select flex items-center justify-center p-2 border border-white rounded-full cursor-pointer w-28 ${
+                        coordinateSystem === "intrinsic" ? "bg-blue-600 text-white" : "bg-white text-black"
+                    }`}
+                >
+                    Intrínsecas
+                </label>
+            </div>
+        </div>
+    );
+}
