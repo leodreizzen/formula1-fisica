b603e51c0b90947e92fb1470699807ca95334795
leodreizzen <leodreizzen@gmail.com>
2024-06-16 17:03:47 -0300

    Agregar progress indicator

--- client/src/components/panels/main/accelerations/AccelerationsPanel.js
+++ client/src/components/panels/main/accelerations/AccelerationsPanel.js
@@ -1,13 +1,14 @@
 import MainDriverSelector from "../../../inputs/MainDriverSelector";
 import AccelerationPlots from "../../../plots/AccelerationPlots";
 import MainLapSelector from "../../../inputs/MainLapSelector";
-import { CoordinateSystemSelector } from "../../../inputs/CoordinateSystemSelector";
-import { useKinematicVectorsContext } from "../../../../context/KinematicVectorsContext";
+import {CoordinateSystemSelector} from "../../../inputs/CoordinateSystemSelector";
+import {useKinematicVectorsContext} from "../../../../context/KinematicVectorsContext";
 import PolarAccelerationPlot from "../../../plots/PolarAccelerationPlot";
-import { useState } from "react";
+import {useState} from "react";
 import CartesianAccelerationPlot from "../../../plots/CartesianAccelerationPlot";
+import {OrbitProgress} from "react-loading-indicators";
 
-export default function AccelerationsPanel({className}){
+export default function AccelerationsPanel({className}) {
     const [coordinateSystem, setCoordinateSystem] = useState("cartesian");
 
     const {vectors} = useKinematicVectorsContext();
@@ -17,26 +18,31 @@ export default function AccelerationsPanel({className}){
     };
 
     let plots;
-    switch (coordinateSystem) {
-        case "cartesian":
-            plots = <CartesianAccelerationPlot className="grow pt-2 h-2/3" timeUnit={"s"}  vectors={vectors}/>;
-            break;
-        case "polar":
-            plots = <PolarAccelerationPlot className="grow pt-2 h-2/3" timeUnit={"s"}  vectors={vectors}/>;
-            break;
-        case "intrinsic":
-            plots = <AccelerationPlots className="grow pt-2 h-2/3"  timeUnit={"s"}/>;
-            break;
-        default:
-            plots = null;
-            break;
-    }
+    if (vectors) {
+        switch (coordinateSystem) {
+            case "cartesian":
+                plots = <CartesianAccelerationPlot className="grow pt-2 h-2/3" timeUnit={"s"} vectors={vectors}/>;
+                break;
+            case "polar":
+                plots = <PolarAccelerationPlot className="grow pt-2 h-2/3" timeUnit={"s"} vectors={vectors}/>;
+                break;
+            case "intrinsic":
+                plots = <AccelerationPlots className="grow pt-2 h-2/3" timeUnit={"s"}/>;
+                break;
+            default:
+                plots = null;
+                break;
+        }
+    } else plots = <div className="grow pt-2 h-2/3 inline-block"><OrbitProgress size='large'
+                                                                                color="#EFE2E2"
+                                                                                variant='dotted'/></div>
 
     return (
-    <div className={className + " flex flex-col items-center overflow-clip h-full"}>
-        <MainDriverSelector/>
-        {plots}
-        <CoordinateSystemSelector className={"flex"} currentSystem={coordinateSystem} onCoordinateSystemChange={handleCoordinateSystemChange}/>
-        <MainLapSelector className="mb-3 p-1 pl-6 pr-6"/>
-    </div>);
+        <div className={className + " flex flex-col items-center overflow-clip h-full"}>
+            <MainDriverSelector/>
+            {plots}
+            <CoordinateSystemSelector className={"flex"} currentSystem={coordinateSystem}
+                                      onCoordinateSystemChange={handleCoordinateSystemChange}/>
+            <MainLapSelector className="mb-3 p-1 pl-6 pr-6"/>
+        </div>);
 }
